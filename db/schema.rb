0f743d67bc6bf156bb031cc3b1752dc90ae5c2c1
vinicomelo <vinicomelo@gmail.com>
2017-02-22 05:48:51 -0500

    added precision and scale to cost column. Made it show as currency in view.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222090821) do
+ActiveRecord::Schema.define(version: 20170222104217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,10 +54,10 @@ ActiveRecord::Schema.define(version: 20170222090821) do
     t.integer  "destination_id"
     t.integer  "carrier_id"
     t.integer  "shipment_category_id"
-    t.decimal  "cost"
+    t.decimal  "cost",                 precision: 10, scale: 2
     t.integer  "unit_of_measure_id"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
     t.index ["carrier_id"], name: "index_rates_on_carrier_id", using: :btree
     t.index ["destination_id"], name: "index_rates_on_destination_id", using: :btree
     t.index ["origin_id"], name: "index_rates_on_origin_id", using: :btree
