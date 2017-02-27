0ad25179ef0f8d354c75b874b513f72a76b08f95
vinicomelo <vinicomelo@gmail.com>
2017-02-27 18:49:21 -0500

    removed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-           :sessions => "users/sessions",
-           :registrations => "users/registrations" }
   get "/pages/:page" => "pages#index"
   resources :rates
   resources :carrier_types
