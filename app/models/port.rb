fd962881de6eb6fe5241c19ccf03a9dd0c58093b
vinicomelo <vinicomelo@gmail.com>
2017-02-28 10:27:29 -0500

    added table class to Port index and fixed Port model import action

--- app/models/port.rb
+++ app/models/port.rb
@@ -1,5 +1,10 @@
 class Port < ApplicationRecord
-
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      port = find_by_id(row["id"])
+      Port.create! row.to_hash
+    end
+  end
   belongs_to :shipment_type
   belongs_to :country
   has_many :origins, :class_name => 'Rate'
@@ -9,22 +14,20 @@ class Port < ApplicationRecord
   def shipment_type_name
     shipment_type.try(:name)
   end
+
   def shipment_type_name=(name)
     self.shipment_type = ShipmentType.where(:name => name).first_or_create
   end
 
-  def country_country_code
-    country.try(:country_code)
-  end
-  def country_country_code=(country_code)
-    self.country = Country.where(:country_code => country_code).first_or_create
-  end
-
-  def self.import(file)
-    CSV.foreach(file.path, headers: true) do |row|
-      Country.create! row.to_hash
+    def country_country_code
+      country.try(:country_code)
     end
-  end
+
+    def country_country_code=(country_code)
+      self.country = Country.where(:country_code => country_code).first
+    end
+
+
 
 
 end
