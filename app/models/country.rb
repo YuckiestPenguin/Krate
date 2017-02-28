b1f5c603d1c41ec0509542e90ea9d2ff7481ea60
vinicomelo <vinicomelo@gmail.com>
2017-02-28 08:40:40 -0500

    setting up files for CSV Import feature

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,12 +1,7 @@
 class Country < ApplicationRecord
   has_many :ports
 
-  def self.import(file)
-    #code
-    CSV.foreach(file.path, headers: true) do |row|
-      Country.create! row.to_hash
-    end
-  end
+
 
 
 end
