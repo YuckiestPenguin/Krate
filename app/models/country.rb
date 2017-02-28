90aac45d02f791a2cc8736ba3bebc09c9b1a7201
vinicomelo <vinicomelo@gmail.com>
2017-02-28 09:48:02 -0500

    added csv import info to Country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,11 @@
 class Country < ApplicationRecord
   has_many :ports
 
+  def self.import(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      Country.create! row.to_hash
+    end
+  end
 
 
 
