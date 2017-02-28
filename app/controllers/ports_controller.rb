3ae66ba8ea2937169593e2d983fa1248e91c4971
vinicomelo <vinicomelo@gmail.com>
2017-02-28 10:15:30 -0500

    added import action to Port Controller

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -63,6 +63,12 @@ class PortsController < ApplicationController
     end
   end
 
+  def import
+    Port.import(params[:file])
+    redirect_to ports_path, notice: "Ports Imported Successfully!"
+    #code
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_port
