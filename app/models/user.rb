5ca9cae153dc84672110a0097227fcf2a624ee17
vinicomelo <vinicomelo@gmail.com>
2017-03-22 12:22:47 -0400

    fixed email and username login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: false, login: false}
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => {email: false, login: true}
 
          def self.find_for_database_authentication(warden_conditions)
            conditions = warden_conditions.dup
